d7cff36b942892bd38ff0b0df52e0fdc31a3ea10
Dong-Young Kim <31337.cert@gmail.com>
2018-11-17 06:04:14 +0900

    Add about section

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ function loadFile(url) {
 new Vue({
   el: 'main',
   data: {
+    abouts: 0,
     activities: 0,
     awards: 0,
     certifications: 0,
@@ -28,17 +29,19 @@ new Vue({
   },
   created: function() {
     Promise.all([
+      loadFile('data/abouts.json'),
       loadFile('data/activities.json'),
       loadFile('data/awards.json'),
       loadFile('data/certifications.json'),
       loadFile('data/projects.json'),
       loadFile('data/skills.json')
     ]).then(values => {
-      this.activities = JSON.parse(values[0]);
-      this.awards = JSON.parse(values[1]);
-      this.certifications = JSON.parse(values[2]);
-      this.projects = JSON.parse(values[3]);
-      this.skills = JSON.parse(values[4]);
+      this.abouts = JSON.parse(values[0]);
+      this.activities = JSON.parse(values[1]);
+      this.awards = JSON.parse(values[2]);
+      this.certifications = JSON.parse(values[3]);
+      this.projects = JSON.parse(values[4]);
+      this.skills = JSON.parse(values[5]);
     }).catch(reason => {
       console.error('File load error: ' + reason);
     });

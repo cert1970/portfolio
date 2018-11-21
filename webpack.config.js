fca0b03df1c95f932c423f113fa3cf5983cb093e
Dong-Young Kim <31337.cert@gmail.com>
2018-11-22 03:43:06 +0900

    Support webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  entry: ['./resources/scss/style.scss', './resources/js/main.js'],
+  output: {
+    filename: 'public/js/main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'public/css/style.css',
+            },
+          },
+          {loader: 'extract-loader'},
+          {loader: 'css-loader'},
+          {loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer()],
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: ['./node_modules'],
+            },
+          }
+        ],
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+          plugins: ['transform-object-assign']
+        },
+      }
+    ],
+  },
+};

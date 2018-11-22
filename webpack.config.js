ed64358dff51f1081bcb6a8c19bfbe23c85adbbf
Dong-Young Kim <31337.cert@gmail.com>
2018-11-23 05:12:29 +0900

    Update dev settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 'use strict';
 
 const autoprefixer = require('autoprefixer');
+const path = require('path');
 
 module.exports = {
-  entry: ['./resources/scss/style.scss', './resources/js/main.js'],
-  output: {
-    filename: 'public/js/main.js'
+  entry: ['./resources/js/main.js', './resources/scss/style.scss'],
+  devServer: {
+    compress: false,
+    port: 8080
   },
+  output: {
+    filename: 'public/js/bundle.js',
+  },
+  devtool: 'source-map',
   module: {
     rules: [
       {
@@ -19,27 +25,36 @@ module.exports = {
             },
           },
           {loader: 'extract-loader'},
-          {loader: 'css-loader'},
-          {loader: 'postcss-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
             options: {
               plugins: () => [autoprefixer()],
+              sourceMap: true
             },
           },
           {
             loader: 'sass-loader',
             options: {
               includePaths: ['./node_modules'],
+              sourceMap: true
             },
           }
         ],
       },
       {
         test: /\.js$/,
+        exclude: /(node_modules)|(dist)/,
         loader: 'babel-loader',
         query: {
           presets: ['es2015'],
           plugins: ['transform-object-assign']
-        },
+        }
       }
     ],
   },
